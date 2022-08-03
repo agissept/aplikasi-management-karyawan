215278c2a49d4d1077238fd78109663c993c2dfc
agis <agis.septiyanto@gmail.com>
2022-08-04 01:09:19 +0700

    convert time zone

--- handler/Attendances.py
+++ handler/Attendances.py
@@ -1,3 +1,6 @@
+from datetime import timezone
+from pytz import timezone
+
 from flask_restful import Resource
 
 from repositories.attendances import get_attendances_by_employee_id, insert_attendance, check_today_attendance
@@ -7,7 +10,12 @@ from utils.response_handler import response_success, response_error
 class EmployeeAttendance(Resource):
     def get(self, employee_id):
         attendances = get_attendances_by_employee_id(employee_id)
-        return response_success(attendances, 'attendances')
+        attendances = map(lambda attendance: {
+            "id": attendance['id'],
+            "user_id": attendance['user_id'],
+            "created_at": utc_to_local(attendance['created_at'])
+        }, attendances)
+        return response_success(list(attendances), 'attendances')
 
     def post(self, employee_id):
         if check_today_attendance(employee_id) is None:
@@ -16,3 +24,6 @@ class EmployeeAttendance(Resource):
         else:
             return response_error("You have already checked in", 400)
 
+
+def utc_to_local(utc_dt):
+    return utc_dt.astimezone(timezone('Asia/Jakarta'))
