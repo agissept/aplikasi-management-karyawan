d03aa358e2e2fee59387f8de4b977df4f6aa20d4
agis <agis.septiyanto@gmail.com>
2022-08-08 22:09:08 +0700

    fill all indicator

--- public/src/script/ui/dashboard.js
+++ public/src/script/ui/dashboard.js
@@ -1,13 +1,21 @@
-import {getPaidLeavesByUserId, getUserByUserId} from "../repository/api-helper.js";
+import {getTimeOffByUserId, getUserByUserId, insertPaidLeave} from "../repository/api-helper.js";
 
 window.onload = async () => {
     const user = await getUser()
 
-    const paidLeaves = await getPaidLeaves(user.id)
-    renderTable(paidLeaves)
+    const timeOff = await getTimeOff(user.id)
+    const paidLeaves = timeOff.filter(timeOff => timeOff.policy === 'Cuti')
+    const dinas = timeOff.filter(timeOff => timeOff.policy === 'Dinas')
+    const lembur = timeOff.filter(timeOff => timeOff.policy === 'Lembur')
+    const sick = timeOff.filter(timeOff => timeOff.policy === 'Sakit')
+
+    renderTable(timeOff)
 
     document.querySelector('#rest_paid_leaves').innerHTML = 12 - paidLeaves.length
     document.querySelector('#taken_paid_leaves').innerHTML = paidLeaves.length
+    document.querySelector('#amount-dinas').innerHTML = dinas.length
+    document.querySelector('#amount-lembur').innerHTML = lembur.length
+    document.querySelector('#amount-sick').innerHTML = sick.length
 }
 
 async function getUser() {
@@ -20,7 +28,7 @@ async function getUser() {
 
 async function renderTable(paidLeaves) {
     const container = document.querySelector('#paid-leaves-table')
-    console.log(paidLeaves)
+    container.innerHTML = ""
     paidLeaves.forEach(paidLeave => {
         const row = document.createElement('tr')
         row.innerHTML = `
@@ -33,7 +41,45 @@ async function renderTable(paidLeaves) {
     })
 }
 
-async function getPaidLeaves(userId) {
-    const takenPaidLeaves = await getPaidLeavesByUserId(userId)
-    return takenPaidLeaves.data.paid_leaves
+async function getTimeOff(userId) {
+    const takenPaidLeaves = await getTimeOffByUserId(userId)
+    return takenPaidLeaves.data.time_off
 }
+
+function isPaidLeavePayloadAreValid(startDate, endDate, reason) {
+    if (reason === '' || startDate === '' || endDate === '') {
+        alert('Please fill all the fields')
+        return false
+    }
+
+    if (startDate > endDate) {
+        alert('Start date must be less than end date')
+        return false
+    }
+
+    return true
+}
+
+document.querySelector('#btn-submit').addEventListener('click', async (event) => {
+    event.preventDefault()
+    const user = await getUser()
+
+    const startDate = document.querySelector('#start-date').value
+    const endDate = document.querySelector('#end-date').value
+    const reason = document.querySelector('#reason').value
+    const userId = user.id
+    const policy = document.querySelector('#policy').value
+
+
+    if(!isPaidLeavePayloadAreValid(startDate, endDate, reason, userId)){
+        return false
+    }
+    console.log(startDate, endDate, reason, userId, policy)
+    try {
+        await insertPaidLeave(userId, startDate, endDate, reason, policy)
+        const paidLeaves = await getTimeOff(user.id)
+        renderTable(paidLeaves)
+    } catch (e) {
+        alert(e)
+    }
+})
