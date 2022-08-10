406a9fe6c17cbd8271242a5ff417c50a5b5897d9
agis <agis.septiyanto@gmail.com>
2022-08-10 21:14:13 +0700

    fix style

--- public/src/script/component/navigation/navigation.js
+++ public/src/script/component/navigation/navigation.js
@@ -1,7 +1,6 @@
 const baseUrl = 'src/script/component/navigation';
 fetch(`${baseUrl}/navigation.html`).then(response => response.text()).then(html => {
     document.getElementById('navigation').innerHTML = html;
-    document.head.innerHTML += `<link rel="stylesheet" href="${baseUrl}/navigation.css">`
 
 
     document.querySelector('#logout').addEventListener('click', function (e) {
