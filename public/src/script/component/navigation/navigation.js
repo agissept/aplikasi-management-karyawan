2dc4220958b49d0a7dfba3053df08bdfe9d3bc83
agis <agis.septiyanto@gmail.com>
2022-08-10 20:18:50 +0700

    add logout function

--- public/src/script/component/navigation/navigation.js
+++ public/src/script/component/navigation/navigation.js
@@ -2,4 +2,14 @@ const baseUrl = 'src/script/component/navigation';
 fetch(`${baseUrl}/navigation.html`).then(response => response.text()).then(html => {
     document.getElementById('navigation').innerHTML = html;
     document.head.innerHTML += `<link rel="stylesheet" href="${baseUrl}/navigation.css">`
+
+
+    document.querySelector('#logout').addEventListener('click', function (e) {
+        e.preventDefault()
+        console.log('sdsdsd')
+        sessionStorage.removeItem('userId')
+        window.location.href = '/'
+    })
 })
+
+
